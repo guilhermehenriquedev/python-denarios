36c71fc1d3b546806d7b2e7b6e44a1bab7ff8be3
Guilherme Henrique <guilhermebass27@gmail.com>
2021-10-23 14:45:39 -0300

    Adiciona IP do server

--- back/denarios/settings.py
+++ back/denarios/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-305n6d$)+$wuri=^52**!%xwk_+9aax*n6t-#5#p3xmd35r_$*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.0.107', config('SERVER', default='127.0.0.1')]
 
 
 # Application definition
