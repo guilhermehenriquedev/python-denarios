90c96c8338afcdbd8c1f5d5b0b438934d8d3a9f7
Guilherme Fernandes <guilherme.fernandes@paraiba.email>
2022-02-01 17:54:01 -0300

    corrige chosen cryptos

--- denarios/core/helpers/criptos.py
+++ denarios/core/helpers/criptos.py
@@ -1,19 +1,19 @@
 def criptos(par=None):
 
     criptos = [
-    'ETH' + par if par else '',
-    'ETC' + par if par else '',
-    'DOGE' + par if par else '',
-    'XRP' + par if par else '',
-    'ADA' + par if par else '',
-    'BNB' + par if par else '',
-    'USDT' + par if par else '',
-    'BCH' + par if par else '',
-    'EOS' + par if par else '',
-    'DOT' + par if par else '',
-    'AXS' + par if par else '',
-    'PAX' + par if par else '',
-    'SOL' + par if par else '',
+    'ETH' + par if par else 'ETH',
+    'ETC' + par if par else 'ETC',
+    'DOGE' + par if par else 'DOGE',
+    'XRP' + par if par else 'XRP',
+    'ADA' + par if par else 'ADA',
+    'BNB' + par if par else 'BNB',
+    'USDT' + par if par else 'USDT',
+    'BCH' + par if par else 'BCH',
+    'EOS' + par if par else 'EOS',
+    'DOT' + par if par else 'DOT',
+    'AXS' + par if par else 'AXS',
+    'PAX' + par if par else 'PAX',
+    'SOL' + par if par else 'SOL',
     ]
 
     return criptos
