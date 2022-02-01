7e81842402bd487c59e7ba49762ea5bd8fe97ef8
Guilherme Fernandes <guilherme.fernandes@paraiba.email>
2022-02-01 09:42:38 -0300

    adiciona api brasil bitcoin

--- denarios/core/usecases/exchanges.py
+++ denarios/core/usecases/exchanges.py
@@ -13,6 +13,7 @@ class Exchanges:
             'Content-Type': 'application/json'
         } 
     
+
     def binance(self, headers=None, par_crypt=None):
         
         try:
@@ -33,10 +34,33 @@ class Exchanges:
 
         return data if data else {'message': 'Sem dados para exibir'} 
    
-    #TODO: colocar apis abaixo
-    def brasil_bitcoin(self):
-        pass 
 
+    def brasil_bitcoin(self, headers=None):
+         
+        data_brasil_bitcoin = []
+        chosen_crypts = criptos()
+        for crypto in chosen_crypts:
+            try:
+                url      = BRASIL_BITCOIN_API_URL + f"/otc/orderbook/{crypto}"
+                response = requests.request("GET", url, headers=headers)
+                data     = response.json()
+
+                data_brasil_bitcoin += [{
+                    'no_cripto': crypto,
+                    'vl_venda': data['sell']['preco'],
+                    'vl_compra' data['buy']['preco']:
+                }]
+
+            except Exception as err:
+                data_brasil_bitcoin += [{
+                    'no_cripto': crypto,
+                    'vl_venda': '---',
+                    'vl_compra' '---':
+                }]
+                continue
+
+        return data_brasil_bitcoin
+    
     def bitcoin_trade(self):
         pass
 
@@ -51,7 +75,7 @@ class Exchanges:
         start_time = time.time()
 
         binance = self.binance(headers=self.headers, par_crypt='BRL')
-        brasil_bitcoin = self.brasil_bitcoin()
+        brasil_bitcoin = self.brasil_bitcoin(headers=self.headers)
 
         data = {
                 'binance': binance,
