be00fe6014e032a78367a3d8b15c560f472e9b95
Guilherme Henrique <guilhermebass27@gmail.com>
2022-03-24 00:31:46 -0300

    faz as api ordenadas

--- denarios/core/usecases/exchanges.py
+++ denarios/core/usecases/exchanges.py
@@ -14,73 +14,53 @@ class Exchanges:
         } 
     
 
-    def binance(self, headers=None, par_crypt=None):
-        
+    def binance(self, headers=None, par_crypt=None, crypt=None):
+
         try:
-            url      = BINACE_API_URL + "/api/v3/ticker/price" 
+            crypt_get = crypt + par_crypt
+            url      = BINACE_API_URL + f"/api/v3/ticker/price?symbol={crypt_get}" 
             response = requests.request("GET", url, headers=headers)
             data     = response.json()
 
+            data_binance = {
+                'exchange': 'Binance',
+                'vl_venda': round(float(data['price']), 2),
+                'vl_compra': round(float(data['price']), 2)
+            } 
+
         except Exception as err:
-            return {'message': 'Erro ao fazer GET'}
+            data_binance = {
+                'exchange': 'Binance',
+                'vl_venda': 'NDA',
+                'vl_compra': 'NDA'
+            } 
 
-        chosen_crypts = criptos(par=par_crypt)
-
-        data = [{
-            'no_cripto': item['symbol'][:-3],
-            'vl_venda': round(float(item['price']), 2),
-            'vl_compra': round(float(item['price']), 2)
-            } for item in data if item['symbol'] in chosen_crypts]
-
-        return data if data else {'message': 'Sem dados para exibir'} 
+        return data_binance 
    
-    def brasil_bitcoin(self, headers=None):
+    def brasil_bitcoin(self, headers=None, crypt=None):
          
-        data_brasil_bitcoin = []
-        chosen_crypts = criptos()
-        for crypto in chosen_crypts:
-            print('crypto br......: ', crypto)
-            try:
-                url      = BRASIL_BITCOIN_API_URL + "/otc/orderbook/" + crypto
-                response = requests.request("GET", url, headers=headers)
-                data     = response.json()
+        try:
+            url      = BRASIL_BITCOIN_API_URL + "/otc/orderbook/" + crypt
+            response = requests.request("GET", url, headers=headers)
+            data     = response.json()
 
-                data_brasil_bitcoin += [{
-                    'no_cripto': crypto,
-                    'vl_venda': round(float(data['sell']['preco']), 2),
-                    'vl_compra': round(float(data['buy']['preco']), 2)
-                }]
+            data_brasil_bitcoin = {
+                'exchange': 'Brasil Bitcoin',
+                'vl_venda': round(float(data['sell']['preco']), 2),
+                'vl_compra': round(float(data['buy']['preco']), 2)
+            }
 
-            except Exception as err:
-                data_brasil_bitcoin += [{
-                    'no_cripto': crypto,
-                    'vl_venda': 'NDA',
-                    'vl_compra': 'NDA'
-                }]
-                continue
+        except Exception as err:
+            data_brasil_bitcoin = {
+                'Exchange': 'Brasil Bitcoin',
+                'vl_venda': 'NDA',
+                'vl_compra': 'NDA'
+            }
 
         return data_brasil_bitcoin
     
     def nova_dax(self, headers=None):
-        data_novadax = []
-        chosen_crypts = criptos()
-        for crypto in chosen_crypts:
-            try:
-                url = NOVADAX_API_URL + f"/market/trades?symbol={crypto}_BRL&limit=2"
-                response = requests.request("GET", url, headers=headers)
-                data     = response.json()
-                print('data....> ', data)
-                data_novadax += [{
-                    'no_cripto': crypto,
-                    'vl_venda': 'NDA',
-                    'vl_compra': 'NDA'
-                }
-                ]
-                print('data....: ', data)
-            except Exception as err:
-                continue
-        return data_novadax
-
+        pass
 
     def mercado_bitcoin(self):
         pass
@@ -90,21 +70,13 @@ class Exchanges:
 
     def execute(self):
 
-        start_time = time.time()
+        chosen_crypts = criptos() 
+        data = []
+        for crypt in chosen_crypts:
+            binance = self.binance(headers=self.headers, par_crypt='BRL', crypt=crypt)
+            brasil_bitcoin = self.brasil_bitcoin(headers=self.headers, crypt=crypt)
 
-        binance = self.binance(headers=self.headers, par_crypt='BRL')
-        brasil_bitcoin = self.brasil_bitcoin(headers=self.headers)
-        nova_dax = self.nova_dax(headers=self.headers)
-
-        data = {
-                'binance': binance,
-                'brasilbitcoin': brasil_bitcoin,
-                'bitcoin_trade': '',
-                'nova_dax': nova_dax
-                }
-
-        elapsed_time = time.time() - start_time
-        print(f'Tempo....: {time.strftime("%H:%M:%S", time.gmtime(elapsed_time))}')
+            data += [{crypt: [binance, brasil_bitcoin,]}]
 
         return data 
 
